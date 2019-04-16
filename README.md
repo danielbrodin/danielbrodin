f9dd8b74e63fcb8ff4b494e3ca22f4decba06f3f
Daniel Brodin <daniel@itiden.se>
2019-04-16 09:58:22 +0200

    Update with own data

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-It's me, Tierney!
+It's me, Daniel!
 
 # Usage
 Via npx:
 ```
-npx bitandbang
+npx danielbrodin
 ```
 
 Not including how to use it globally because I'm not sure why you'd want this as a global command. That'd be creepy.
--- build.js
+++ build.js
@@ -15,18 +15,20 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('               Tierney Cyren'),
-  handle: chalk.white('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  opensource: chalk.white('Node.js Community Committee ') + chalk.green('⬢'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
-  github: chalk.gray('https://github.com/') + chalk.green('bnb'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.red('npx') + ' ' + chalk.white('bitandbang'),
+  name: chalk.white('               Daniel Brodin'),
+  handle: chalk.white('danielbrodin'),
+  work: chalk.white('Web & mobile developer @ itiden, Gothenburg, Sweden'),
+  workUrl: chalk.yellow('https://itiden.se'),
+  // opensource: chalk.white('Node.js Community Committee ') + chalk.green('⬢'),
+  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('danielbrodin'),
+  npm: chalk.gray('https://npmjs.com/') + chalk.red('~danielbrodin'),
+  github: chalk.gray('https://github.com/') + chalk.green('danielbrodin'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('danielbrodin'),
+  web: chalk.cyan('http://danielbrodin.se'),
+  npx: chalk.red('npx') + ' ' + chalk.white('danielbrodin'),
   labelWork: chalk.white.bold('       Work:'),
-  labelOpenSource: chalk.white.bold('Open Source:'),
+  labelWorkUrl: chalk.white.bold('   Work URL:'),
+  // labelOpenSource: chalk.white.bold('Open Source:'),
   labelTwitter: chalk.white.bold('    Twitter:'),
   labelnpm: chalk.white.bold('        npm:'),
   labelGitHub: chalk.white.bold('     GitHub:'),
@@ -39,7 +41,8 @@ const data = {
 const newline = '\n'
 const heading = `${data.name} / ${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
-const opensourcing = `${data.labelOpenSource}  ${data.opensource}`
+const workingUrl = `${data.labelWorkUrl}  ${data.workUrl}`
+// const opensourcing = `${data.labelOpenSource}  ${data.opensource}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
@@ -51,9 +54,9 @@ const carding = `${data.labelCard}  ${data.npx}`
 const output = heading + // data.name + data.handle
                newline + newline + // Add one whole blank line
                working + newline + // data.labelWork + data.work
-               opensourcing + newline + newline + // data.labelOpenSource + data.opensource
+               workingUrl + newline + newline + // data.labelOpenSource + data.opensource
                twittering + newline + // data.labelTwitter + data.twitter
-               npming + newline + // data.labelnpm + data.npm
+              //  npming + newline + // data.labelnpm + data.npm
                githubing + newline + // data.labelGitHub + data.github
                linkedining + newline + // data.labelLinkedIn + data.linkedin
                webing + newline + newline + // data.labelWeb + data.web
